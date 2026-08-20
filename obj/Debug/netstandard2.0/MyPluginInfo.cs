c92d9d3a1d6e92b1adf2e67fb64b1e261a68dc73
Blue Dog Wx <kailanadam9@gmail.com>
2026-08-20 21:48:07 +0100

    Delete obj directory

--- obj/Debug/netstandard2.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace RandomJumpMod
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "RandomJumpMod";
-        public const string PLUGIN_NAME = "My first plugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
